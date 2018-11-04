f58c1ef6dacea3571f0fcb2e5ab393e8bc32da9f
farrasdoko <farras.abiyyu.doko@gmail.com>
2018-11-04 13:39:04 +0700

    Initial Commit

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public GameObject menuPanel, howToPanel, CreditPanel;
+
+	// Use this for initialization
+	void Start () {
+		menuPanel.SetActive (true);
+		howToPanel.SetActive (false);
+		CreditPanel.SetActive (false);
+	}
+
+	public void OnStartGameClicked() {
+		SceneManager.LoadScene ("MiniGame");
+	}
+
+	public void HowToClicked(){
+		menuPanel.SetActive (false);
+		howToPanel.SetActive (true);
+		CreditPanel.SetActive (false);
+	}
+
+	public void CreditsClicked(){
+		menuPanel.SetActive (false);
+		howToPanel.SetActive (false);
+		CreditPanel.SetActive (true);
+	}
+
+	public void QuitClicked(){
+		Application.Quit();
+//		System.Environment.Exit(1);
+	}
+
+	public void OnBackClicked() {
+		menuPanel.SetActive (true);
+		howToPanel.SetActive (false);
+		CreditPanel.SetActive (false);
+	}
+}
